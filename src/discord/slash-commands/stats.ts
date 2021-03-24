644cc18474f68f164ce4ef5ca36f8a4c72fb2d48
Kristian Rekstad <krirek@msn.com>
2021-03-24 20:21:46 +0100

    Update strings to be english. Remove typos

--- src/discord/slash-commands/stats.ts
+++ src/discord/slash-commands/stats.ts
@@ -9,12 +9,13 @@ const log = logger("statsCommand");
 
 export const statsCommand: SlashCommand = {
   name: "stats",
-  description: "Hvis man ikke skriver inn navn kommer stats på alle spillerne",
+  description: "Get the stats of all players, or a specific player.",
   options: [
     {
       type: 3,
       name: "name",
-      description: "Spilleren du ønsker å se stats til",
+      description:
+        "Name of a specific player. Leave this empty to get all players.",
       default: false,
       required: false,
     },
@@ -28,34 +29,33 @@ export async function registerStatsCommand(client: Client, guildId: string) {
 export const statsCommandHandler: SlashCommandListener = {
   async onCommand(client, interaction) {
     const playerName = interaction.data?.options?.[0].value;
+    // All players
     if (!playerName) {
       try {
         const allPlayers = await api.getRankings();
         const content = allPlayers
           .map(
             (player, index) =>
-              "On " +
-              index +
-              ". We find " +
-              player.name +
-              " with an elo of " +
-              player.elo
+              `On ${index}. we find ${player.name}, with an elo of ${player.elo}.`
           )
           .join("\n");
         return {
           content,
         };
       } catch (err) {
-        log.error({ err }, "Failed to get list %s: %s", err.message);
+        log.error({ err }, "Failed to get players: %s", err.message);
       }
-      return { content: "List not found" };
+      return { content: "Players not found" };
     }
 
+    // A specific player
     try {
       const profile = await api.getPlayerProfile(playerName);
       if (profile) {
         return {
-          content: `${profile.name} sin elo er ${profile.elo} og siste aktivitet var på ${profile.lastActivity}`,
+          content: `${profile.name}'s elo is ${
+            profile.elo
+          }, and the last activity was ${profile.lastActivity.toLocaleString()}`,
         };
       } else {
         return { content: "Failed to find player " + playerName };
--- src/discord/slash-commands/undo-last-match.ts
+++ src/discord/slash-commands/undo-last-match.ts
@@ -23,7 +23,7 @@ export const undoLastMatchHandler: SlashCommandListener = {
   async onCommand(client, interaction) {
     try {
       await api.undoLastGame();
-      return { content: "Last match deleted" };
+      return { content: "Last match deleted." };
     } catch (err) {
       log.error({ err }, "Failed to register name: %s", err.message);
       return { content: "Failed to register the game: " + err.message };
