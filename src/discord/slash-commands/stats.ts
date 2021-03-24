7be66f7f783eda48c51ea86237f80079597131b7
Anders Pedersen <andped@stud.ntnu>
2021-03-24 20:28:12 +0100

    Added general stats

--- src/discord/slash-commands/stats.ts
+++ src/discord/slash-commands/stats.ts
@@ -14,7 +14,8 @@ export const statsCommand: SlashCommand = {
     {
       type: 3,
       name: "name",
-      description: "Spilleren du ønsker å se stats til",
+      description:
+        "Spilleren du ønsker å se stats til, skriv generell for generell stats",
       default: false,
       required: false,
     },
@@ -49,6 +50,26 @@ export const statsCommandHandler: SlashCommandListener = {
         log.error({ err }, "Failed to get list %s: %s", err.message);
       }
       return { content: "List not found" };
+    }
+
+    if (playerName == "general") {
+      try {
+        const stats = await api.stats();
+        return {
+          content:
+            `Total amount of games played ${stats.gamesPlayed} \n` +
+            `----------------------- \n` +
+            `The one with highest elo of all times is ${stats.highestElo?.name} with an elo of ${stats.highestElo?.elo} on the ${stats.highestElo?.time}  \n` +
+            `----------------------- \n` +
+            `The one with the lowest elo of all times is ${stats.lowestElo?.name} with an elo of ${stats.lowestElo?.elo} on the ${stats.lowestElo?.time} \n` +
+            `----------------------- \n` +
+            `The biggest upset was ${stats.biggestUpset?.winners} vs ${stats.biggestUpset?.losers} where ${stats.biggestUpset?.winners} won against ${stats.biggestUpset?.losers}. The elo difference was ${stats.biggestUpset?.eloDifference} and ${stats.biggestUpset?.winners} had a ${stats.biggestUpset?.probability} ` +
+            `probability to win. This happened on the ${stats.biggestUpset?.time} \n` +
+            `----------------------- \n`,
+        };
+      } catch (err) {
+        log.error({ err }, "Failed get stats: %s", err.message);
+      }
     }
 
     try {
